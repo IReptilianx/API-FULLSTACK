b5ab1ce5ba63a08bad033129147d661e862c7c72
Ramon Ramirez <ramonrrxd@gmail.com>
2025-06-05 11:22:41 -0600

    Convert process.env.PORT to a number and update server.listen to bind to 0.0.0.0

--- SERVER/src/index.ts
+++ SERVER/src/index.ts
@@ -1,7 +1,7 @@
 import server from "./server";
 
-const PORT = process.env.PORT || 9090;
-server.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 9090; // Convert process.env.PORT to a number
+server.listen(PORT, '0.0.0.0', () => {
   console.log('Debugger attached.');
   console.log(`REST API en el puerto: ${PORT}`);
   console.log(`http://localhost:${PORT}`);
