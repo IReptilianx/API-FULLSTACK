7d7b70dafae8ff4e1f992d2754952343caaab1ab
Ramon Ramirez <ramonrrxd@gmail.com>
2025-06-07 10:47:20 -0600

    Update server port configuration to 8080 for consistency across client and server

--- SERVER/src/index.ts
+++ SERVER/src/index.ts
@@ -1,6 +1,6 @@
 import server from "./server";
 
-const PORT = process.env.PORT || 9090;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log('Debugger attached.');
   console.log(`REST API en el puerto: ${PORT}`);
